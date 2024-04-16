471fdfc1690fddc7e1ad528ea6ae19ef13615271
Rafael <rmartin65@gmail.com>
2024-04-16 11:56:04 +0100

    delete duplicated folder

--- Ejercicio2 - KFC/controlador/controlador_articulos.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once "../modelo/productos.php";
-    $datos = new Articulos();
-    $jsondata["datos"] = $datos->listar();
-    echo json_encode($jsondata, JSON_FORCE_OBJECT);
-?>    
